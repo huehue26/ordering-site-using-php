55339c274925fc4afcd4bbca843529512620021a
Somesh Singh <someshsingh298@gmail.com>
2021-12-05 11:56:18 +0530

    huehue

--- food.php
+++ food.php
@@ -22,7 +22,30 @@ if (isset($_POST['add_to_cart']) != 0) {
     <link href="assets/css/index.css" rel="stylesheet">
     <script src="assets/js/source.bootstrap.min.js"></script>
     <script src="assets/js/jquery.min.js"></script>
+    <link rel="stylesheet" href="css/food2.css">
     <title>Foodie</title>
+    <style>
+        .container-food {
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            flex-wrap: wrap;
+        }
+
+        .food-child {
+            flex-basis: 30%;
+            row-gap: 10px;
+            column-gap: 10px;
+        }
+
+        .food-img {
+            margin: auto;
+        }
+
+        .food-inp {
+            width: 40%;
+        }
+    </style>
 </head>
 
 <body>
@@ -37,21 +60,26 @@ if (isset($_POST['add_to_cart']) != 0) {
     $result = mysqli_query($conn, $sql);
     $row = mysqli_fetch_array($result);
     ?>
-
-    <h4 style="margin-left: 200px;">
+    <div class="container-food">
+        <!-- <h4 style="margin-left: 200px;"> -->
         <?php do {
             $quantity = 0;
-            echo "<div>";
-            echo '<img src="data:images/jpeg;base64,' . base64_encode($row["image"]) . '" height="180" width="180" />';
+            echo "<div class='food-child'>";
+            echo '<img class="food-img" src="data:images/jpeg;base64,' . base64_encode($row["image"]) . '" height="180" width="180" />';
             echo "<br>";
+            echo "<b>Product Name </b>: ";
             echo $row["product_name"];
             echo "<br>";
+            echo "<b>Price </b>: ";
             echo $row["unit_price"];
             echo "<br>";
+            echo "<b>Description </b>: ";
+            echo "<div class=desc-food>";
             echo $row["description"];
+            echo "</div>";
             echo "<br>";
-            echo 'Quantity : <form action="food.php" method="POST"> 
-                                <input type="number" name="quantity">
+            echo '<i><b>Enter Quantity</b></i> : <form action="food.php" method="POST"> 
+                                <input type="number" class="food-inp" name="quantity">
                                 <input type="submit" value="Add to Cart" name="add_to_cart">
                                 <input name="product_id" style="display:none" value="' . $row["product_id"] . '">
                              </form>';
@@ -61,7 +89,10 @@ if (isset($_POST['add_to_cart']) != 0) {
             echo "</div>";
         } while ($row = mysqli_fetch_array($result));
         ?>
-    </h4>
+        <!-- </h4> -->
+    </div>
+    <link rel="stylesheet" href="css/food2.css">
+
 </body>
 
 </html>
--- nav.php
+++ nav.php
@@ -1,68 +1,129 @@
-  <?php
+<?php
+// session_start();
+$status = session_status();
+if ($status == PHP_SESSION_NONE) {
+  //There is no active session
   session_start();
-  require_once 'connect.php';
-  ?>
-  <!DOCTYPE html>
-  <html>
+} else
+if ($status == PHP_SESSION_DISABLED) {
+  //Sessions are not available
+} else
+if ($status == PHP_SESSION_ACTIVE) {
+  //Destroy current and start new one
+  session_destroy();
+  session_start();
+}
+require_once 'connect.php';
+?>
+<!DOCTYPE html>
+<html>
 
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link href="assets/css/half-slider.css" rel="stylesheet">
-    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
-    <link href="assets/css/half-slider.css" rel="stylesheet">
-    <link href="assets/css/index.css" rel="stylesheet">
-    <script src="assets/js/source.bootstrap.min.js"></script>
-    <script src="assets/js/jquery.min.js"></script>
-    <title>Foodie</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <title>Foodie</title>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/food.css">
+
+</head>
+
+<body>
+
+  <nav id="food_nav" style="background-color: #506179!important;" class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="food.php">Foodie</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="food.php">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="about.html">About Us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Contact Us</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li> -->
+
+      </ul>
+      <form class="form-inline my-2 my-lg-0">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
 
 
-  </head>
 
-  <body>
-    <nav class="navbar fixed-top">
-      <div class="container">
-        <a class="navbar-brand" href="index.php">Foodie</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="nav navbar-nav">
-            <li>
-              <a href="index.php">Home </a>
-            </li>
-            <li><a>About Us</a>
-            </li>
-            <li><a>Contact Us</a> </li>
-          </ul>
+      <?php
 
-          <?php
+      if (isset($_SESSION['valid']) != true) {
+      ?>
+        <ul class="nav navbar-nav navbar-right">
+          <a href="sign_in.php"><button type="button" style="padding:3px;margin-right:3px;background-color: #8db0d6d4;" class="btn btn-primary">Sign In</button></a>
+          <a href="sign_up.php"><button type="button" style="padding:3px;margin-right:3px;background-color: #8db0d6d4;" class="btn btn-primary">Sign Up</button></a>
+        </ul>
+      <?php
+      } else {
+      ?>
+        <ul class="nav navbar-nav navbar-right">
+          <a href="profile.php"> <button type="button" style="padding:3px;margin-right:3px;background-color: #8db0d6d4;" class="btn btn-primary">Profile</button></a>
+          <a href="logout.php"><button type="button" style="padding:3px;margin-right:3px;;background-color: #8db0d6d4;" class="btn btn-primary">Logout</button></a>
+        </ul>
+      <?php
+      }
+      ?>
 
-          if (isset($_SESSION['valid']) != true) {
-          ?>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="sign_in.php">Sign In</a></li>
-              <li><a href="sign_up.php">Sign Up</a></li>
-            </ul>
-          <?php
-          } else {
-          ?>
-            <ul class="nav navbar-nav navbar-right">
-              <li><a href="profile.php">Profile</a></li>
-              <li><a href="logout.php">Logout</a></li>
-            </ul>
-          <?php
-          }
-          ?>
 
+    </div>
+  </nav>
+
+  <div class="container-fluid mt-1">
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+      <ol class="carousel-indicators">
+        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+      </ol>
+      <div class="carousel-inner">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="https://source.unsplash.com/1600x500/?orders" class="d-block" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://source.unsplash.com/1600x500/?delivery" class="d-block" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="https://source.unsplash.com/1600x500/?discount" class="d-block" alt="...">
+          </div>
         </div>
       </div>
-    </nav>
-  </body>
+      <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
 
-  </html>
+</html>
